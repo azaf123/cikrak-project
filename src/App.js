b22dc92f12ed2fdc9f6d443207ba39698b6943a3
ifentius ciputra <95.ifentiusciputra@gmail.com>
2022-06-15 17:34:58 +0800

    Navbar and footerresponsive  tailwind first try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navbar/Navbar'
+import Footer from "./components/Footer/Footer";
 
-function App() {
+const App = () =>
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test branch ifent
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Nav />
+      <div className="bg-white-600 w-full h-screen"></div>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
