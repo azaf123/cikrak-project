c2159092c0b6bd62860613d4f1d3ef1f5c6f1f6b
rezkykukuh <rezkykukuh@gmail.com>
2022-07-03 14:05:43 +0700

    Add Horizontal Scroll in card.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -22,7 +22,7 @@ return(
     <section className="container "> 
         <h4  className=" text-2xl font-bold mb-3 ">List Voucher</h4>
         <h6 className="font-bold text-xl sticky mx-2 top-0 ">Food & Beverage</h6>
-        <div className=" rounded-xl  h-80  ml-4  overflow-x-auto p-2 bg-blue-100 shadow-inner   ">
+        <div className=" rounded-xl  ml-4  overflow-x-auto p-2 bg-blue-100 shadow-inner   ">
             <div className="flex m-2">
             {voucher.FoodBeverage.map((item,index)=>{
                 return(
@@ -40,14 +40,15 @@ return(
             })}
             </div>
             </div>
+            
             <h6 className="font-bold text-xl sticky mt-6 mx-2 top-0 ">Entertaiment</h6>
-            <div className="rounded-xl text-xl h-80  ml-4 bg-slate-50 p-2 overflow-x-auto shadow-inner">
+            <div className="rounded-xl text-xl ml-4 bg-slate-50 p-2 overflow-x-scroll shadow-inner">
             
             <div className="flex m-2">
             {voucher.Entertaiment.map((item,index)=>{
                 return(
-                    <div className="col-4 my-2 cursor-pointer z-0 " key={index}> 
-                    <div className="card rounded-xl inline-block w-9/12  p-0 overflow-hidden shadow bg-white" onClick={() => getVoucher
+                    <div className="col-4 my-2  z-0 " key={index}> 
+                    <div className="card rounded-xl cursor-pointer inline-block w-9/12  p-0 overflow-hidden shadow bg-white" onClick={() => getVoucher
                     (item.image, item.voucher, item.date, item.points)}>
                         <img src={item.image} className="card-img-top h-40" /> 
                         <div className="card-body">
@@ -61,7 +62,7 @@ return(
             </div>
             </div>
             <h6 className="font-bold text-xl sticky mt-6 mx-2 top-0">Healthy&Beauty</h6>
-            <div className="rounded-xl text-xl h-80  ml-4 p-2 overflow-x-auto bg-blue-100 shadow-inner">
+            <div className="rounded-xl text-xl  ml-4 p-2 overflow-x-auto bg-blue-100 shadow-inner">
             
             <div className="flex m-2">
             {voucher.HealthyBeauty.map((item,index)=>{
