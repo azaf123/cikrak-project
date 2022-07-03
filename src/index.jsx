ca280394a3f912aee7d21481f7c8a095ac5f9ab7
ifentius ciputra <95.ifentiusciputra@gmail.com>
2022-07-03 18:16:53 +0800

    Update UI register and login

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </Provider>
-)
+  </Provider>,
+  document.getElementById('root')
+);
